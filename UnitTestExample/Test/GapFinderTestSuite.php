09c03925d9edeff6c612c4827595fb9e5bdcc788
cfmack <cfmack@gmail.com>
2010-10-10 21:28:55 -0500

    Adding GapFinderSuite

---
+++ UnitTestExample/Test/GapFinderTestSuite.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'Test\GapFinderUnitTest.php';
+
+require_once 'Test\GapVOUnitTest.php';
+
+/**
+ * Static test suite.
+ */
+class GapFinderTestSuite extends PHPUnit_Framework_TestSuite {
+	
+	/**
+	 * Constructs the test suite handler.
+	 */
+	public function __construct() {
+		$this->setName ( 'GapFinderTestSuite' );
+		
+		$this->addTestSuite ( 'GapFinderUnitTest' );
+		
+		$this->addTestSuite ( 'GapVOUnitTest' );
+	
+	}
+	
+	/**
+	 * Creates the suite.
+	 */
+	public static function suite() {
+		return new self ();
+	}
+}
+
