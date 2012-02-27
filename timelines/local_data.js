b1ebbc3084920ac3260f1f0d5c69863d2d5f83a4
cfmack <cfm4023@yahoo.com>
2012-02-27 12:44:01 -0600

    Figured out the timestamps

--- timelines/local_data.js
+++ timelines/local_data.js
@@ -4,15 +4,15 @@ var timeline_data = {  // save as a global variable
 'wikiSection': "Simile Cubism Timeline",
 
 'events' : [
-        {'start': '2012-02-27',
-        'title': '1 bubble',
-        'description': 'distress bubble',
+        {'start': '2012-02-27T00:10:00',
+        'title': 'bubble',
+        'description': 'distress bubble; 10 minute later',
         'image': 'http://images.allposters.com/images/AWI/NR096_b.jpg',
         'link': 'http://www.allposters.com/-sp/Barfusserkirche-1924-Posters_i1116895_.htm'
         },
 
 
-		{'start': '2012-02-27',
+		{'start': '2012-02-26T23:10:00',
         'title': '2 bubble',
         'description': 'distress bubble',
         'image': 'http://images.allposters.com/images/AWI/NR096_b.jpg',
@@ -20,8 +20,8 @@ var timeline_data = {  // save as a global variable
 		'icon' : "dark-red-circle.png"
         },
 
-        {'start': '2012-02-27',
-        'end': '2012-02-28',
+        {'start': '2012-02-26T23:50:00',
+        'end': '2012-02-27T00:05:00',
         'title': 'screen capture',
         'description': 'screen capture by x',
         'image': 'http://images.allposters.com/images/mer/1336_b.jpg',
