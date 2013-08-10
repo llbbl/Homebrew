980c416d1bdaf8322eaa01a981f4eb0614848f2f
cmack <cfm0423@yahoo.com>
2013-08-10 12:49:53 -0500

    Writing to the Environment table to track allergies Still persiting the raw file that I'm parsing

---
+++ AllergyScrapper/class/Parser.php
@@ -0,0 +1,122 @@
+<?php
+// load mininal zend framework for DOM library
+require_once('./library/ZendFramework-2.2.2/library/Zend/Dom/Query.php');
+require_once('./library/ZendFramework-2.2.2/library/Zend/Dom/Css2Xpath.php');
+require_once('./library/ZendFramework-2.2.2/library/Zend/Dom/NodeList.php');
+require_once('./library/ZendFramework-2.2.2/library/Zend/Stdlib/ErrorHandler.php');
+
+/*
+// if it is an exception, we'll get a "can't find class" error - no reason to go down the crazy path of Zend includes
+require_once('./library/ZendFramework-2.2.2/library/Zend/Dom/BadMethodCallException.php');
+require_once('./library/ZendFramework-2.2.2/library/Zend/Dom/ExceptionInterface.php');
+require_once('./library/ZendFramework-2.2.2/library/Zend/Dom/RuntimeException.php');
+*/
+
+class Parser
+{
+	private $file;
+
+	public function __construct($file)
+	{
+		$this->file = $file;
+	}
+	
+	/**
+	 * Parse out the allergens 
+	 * @return array
+	 */
+	public function parse()
+	{
+		$contents = file_get_contents($this->file);
+		
+		$dom = new Zend\Dom\Query();
+		$dom->setDocument($contents);
+		
+		// @var array  $results of DOMElement
+		$results = $dom->execute('#allergyforecastcontent p');
+		$fragment = $this->DOMinnerHTML($results[1]);  // should be the 2nd <p>
+		$allergens = $this->extract($fragment);
+		
+		return $allergens;
+	}
+	
+	/**
+	 * Extract the innerHTML of the DOMElement
+	 * @param DOMElement $element
+	 * @return string
+	 */
+	private function DOMinnerHTML($element)
+	{
+		$innerHTML = "";
+		$children = $element->childNodes;
+		foreach ($children as $child)
+		{
+			$tmp_dom = new DOMDocument();
+			$tmp_dom->appendChild($tmp_dom->importNode($child, true));
+			$innerHTML.=trim($tmp_dom->saveHTML());
+		}
+		return $innerHTML;
+	}
+	
+	/**
+	 * @test I should really unit test this function!
+	 * @param string $fragment
+	 */
+	private function extract($fragment) 
+	{
+		// Allergy Count: Molds High.&nbsp; This allergy forecast ...
+		/*
+		 // Example parse:
+		
+		Oak Medium 33 gr/m3, Molds Medium
+		Molds High
+		Ash Medium 20 gr/m3, Elm Medium 27 gr/m3, Oak Medium 27 hr/m3, Molds High
+		Trees High 107 gr/m3, Grass High 47 gr/m3, Oak High 1121 gr/m3, Molds High
+		Cedar Low 7 gr/m3, Oak Low 7 gr/m3, Ash High 93 gr/m3, Elm Medium 20 gr/m3, Molds Medium
+		
+		*/
+		
+		$ex = explode('Allergy Count:', $fragment, 2);
+		$fragment = trim($ex[1]);
+		
+		$ex = explode('.', $fragment, 2);
+		$fragment = trim($ex[0]);
+		
+		$ex = explode(',', $fragment);
+		
+		$allergens = array();
+		foreach($ex as $str)
+		{
+			$allergens[] = $this->VOfactory($str);
+		}
+		
+		return $allergens;
+	}
+	
+	/**
+	 * build a Allergen VO by extracting a substring
+	 * 
+	 * Examples:
+	 * - Cedar Low 7 gr/m3
+	 * - Molds Medium
+	 * 
+	 * @param string $str
+	 * @return AllergenVO
+	 */
+	private function VOfactory($str)
+	{
+		$str = trim($str);
+		
+		$ex = explode(' ', $str, 3);
+		
+		$amount = '';
+		if (isset($ex[2]))
+		{
+			$amount = $ex[2];
+		}
+		
+		return new AllergenVO($ex[0], $ex[1], $amount);
+	}
+
+	
+}
--- AllergyScrapper/class/Scrapper.php
+++ AllergyScrapper/class/Scrapper.php
@@ -1,4 +1,7 @@
 <?php
+require_once('./class/Parser.php');
+require_once('./class/AllergenVO.php');
+
 
 class Scrapper 
 {
@@ -23,14 +26,35 @@ class Scrapper
 		$this->outFile = $fileName;
 	}
 	
+	/**
+	 * Controller for Scrapper
+	 * 
+	 */
 	public function scrap()
 	{
 		$this->retrieve();
-		$this->save();
+		$allergens = $this->parse();
+		$this->save($allergens);
 		$this->cleanup();
 	}
 	
-	public function save() 
+	/**
+	 * Encapsulate the parsing
+	 * 
+	 */
+	public function parse()
+	{
+		$p = new Parser($this->outFile);
+		return $p->parse();
+	}
+	
+	/**
+	 * Save raw file and allergy info
+	 * 
+	 * @todo break this out to a DAO
+	 * @param array $allergens
+	 */
+	private function save($allergens) 
 	{
 		// configuration
 		$dbhost 	= DB_HOST;
@@ -42,14 +66,82 @@ class Scrapper
 		$conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 		
 		// query
-		$sql = "INSERT INTO PollenFile (FileDate,AllergyReport) VALUES (:now,:file)";
+		$this->save_file($conn);
 		
-		$q = $conn->prepare($sql);
- 		
-		$q->execute(array(':now'=>date("Y-m-d H:i:s"), ':file'=>file_get_contents($this->outFile)));
-		
+ 		if (count($allergens) > 0)
+ 		{
+			foreach($allergens as $vo)
+			{
+				$this->save_environment($conn, $vo);
+			}
+ 		}
 	}
 	
+	/**
+	 * Save the raw file
+	 * 
+	 * @param PDO connection $conn
+	 */
+	private function save_file($conn) 
+	{
+		// query
+		$sql = "INSERT INTO PollenFile (FileDate,AllergyReport) VALUES (:now,:file)";
+		$q = $conn->prepare($sql);
+		$q->execute(array(':now'=>date("Y-m-d H:i:s"), ':file'=>file_get_contents($this->outFile)));
+	}	
+	
+	
+	
+	/**
+	 * Save into the Environment table
+	 * 
+	 * @param PDO $conn
+	 * @param AllergenVO $allergenVO
+	 */
+	private function save_environment($conn, $allergenVO)
+	{
+		$id = $this->save_environment_type($conn, $allergenVO->get_type());
+		$note = $allergenVO->get_category() . " " . $allergenVO->get_amount(); // @todo - break this up
+		
+		$sql = "INSERT INTO Environment (EnvironmentTypeId,EnvironmentDate, EnvironmentNote ) VALUES (:id, :now,:note)";
+		$q = $conn->prepare($sql);
+		$q->execute(array(':id'=>$id, ':now'=>date("Y-m-d H:i:s"), ':note'=>$note));
+	}
+	
+	/**
+	 * Retrieve EnvironmentTypeId and Save into the Environment Type table
+	 *
+	 * @param PDO $conn
+	 * @param string $type - type of environment
+	 * @return int $id - EnvironmentTypeId for $type
+	 */
+	private function save_environment_type ($conn, $type)
+	{
+		$sql = "SELECT EnvironmentTypeId FROM EnvironmentType WHERE EnvironmentName = :type";
+		
+		$q = $conn->prepare($sql);
+		$q->execute(array(':type'=>$type));
+		$result = $q->fetch(PDO::FETCH_ASSOC);
+		
+		if (!isset($result['EnvironmentTypeId']) )
+		{
+			$sql = "INSERT INTO EnvironmentType (EnvironmentName) VALUES (:type)";
+			$q = $conn->prepare($sql);
+			$q->execute(array(':type'=>$type));
+			
+			$id = $conn->lastInsertId();
+		}
+		else
+		{
+			$id = $result['EnvironmentTypeId'];
+		}
+		
+		return $id;
+	}
+	
+	/**
+	 * Retrieve URL to parse
+	 */
 	private function retrieve()
 	{
 		$cmd = 'wget --referer="http://www.google.com" --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"';
@@ -58,11 +150,12 @@ class Scrapper
 		exec($cmd);
 	}
 
-	
+	/**
+	 * Delete any temporary files
+	 * 
+	 */
 	private function cleanup()
 	{
 		unlink($this->outFile);
 	}
-	
-	
 }
