c314b1870ea73424da35f21b03fe215557bb847d
cmack <cfm0423@yahoo.com>
2013-08-24 23:16:38 -0500

    Create email alerts for high allergy days

--- AllergyScrapper/runner.php
+++ AllergyScrapper/runner.php
@@ -8,5 +8,5 @@ $s = new Scraper('http://www.kvue.com/weather/allergy-forecast');
 //$s->add_mode(Scraper::MODE_NO_CLEANUP);
 
 $s->attach(new Persister());
-//$s->attach(new Alerter());
+$s->attach(new Alerter());
 $s->scrape();
